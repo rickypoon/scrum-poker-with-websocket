daaf53a872a836ef2e5a3f20c338b038e2dd8c4f
Ricky Poon <ricky.poon@oocl.com>
2020-11-13 03:34:00 +0800

    initialize a springboot app

---
+++ poker_service/src/main/java/com/seekers/scrumpoker/ScrumPokerApplication.java
@@ -0,0 +1,13 @@
+package com.seekers.scrumpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumPokerApplication.class, args);
+	}
+
+}
